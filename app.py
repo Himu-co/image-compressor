b6b524d3e4f4bae23cbf50e9658699079c317097
Himu-co <maniruddindhabak@gmail.com>
2024-12-28 17:14:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,7 @@ def compress_image():
 
 if __name__ == "__main__":
     app.run(debug=True)
+     # Use Render-compatible settings
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)  # Bind to 0.0.0.0 for external access
